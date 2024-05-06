25189d6810dca23a1489527cd1be8d853c481155
Xun Liu <xun@datastrato.com>
2024-05-06 12:46:27 +0800

    [#36] Add Jupyter container into playground (#37)
    1. Currently, Gravitino have own Python client, we need have a Python
    notebook environment to execute and use it.
    So, I think add Jupyter into playground.
    2. Added params `bigdata` and `ai` for `launch-playground.sh` 
    
       + `./launch-playground.sh bigdata`
       + `./launch-playground.sh ai`, Open http://localhost:8888 in you brower
    
    4. Added `gravitino-fileset-sample.ipynb` into jupyter, You can use it
    to explore Gravitino Python client integration HDFS
    
    #36 
    
    <img width="1116" alt="image"
    src="https://github.com/datastrato/gravitino-playground/assets/3677382/38dd394c-46da-48b0-81aa-18fb1c20f388">
    
    ![gravitino-jupyter2](https://github.com/datastrato/gravitino-playground/assets/3677382/ebc184d4-5556-4451-bf25-eaaf054d66bc)

--- launch-playground.sh
+++ launch-playground.sh
@@ -3,7 +3,7 @@
 # Copyright 2023 Datastrato Pvt Ltd.
 # This software is licensed under the Apache License version 2.
 #
-#set -ex
+# set -ex
 playground_dir="$(dirname "${BASH_SOURCE-$0}")"
 playground_dir="$(cd "${playground_dir}">/dev/null; pwd)"
 isExist=`which docker-compose`
@@ -15,8 +15,20 @@ else
   exit
 fi
 
+components=""
+case "${1}" in
+  bigdata)
+    components="hive gravitino trino postgresql mysql spark"
+    ;;
+  ai)
+    components="hive gravitino mysql jupyter"
+    ;;
+  *)
+    components=$@
+esac
+
 cd ${playground_dir}
-docker-compose up
+docker-compose up ${components}
 
 # Clean Docker containers when you quit this script
 docker-compose down
