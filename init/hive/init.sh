25c650703f7595e787085f1ee554285d915cdd40
Eric Chang <e850506@gmail.com>
2024-11-27 07:09:04 +0800

    fix: revert init.sh

--- init/hive/init.sh
+++ init/hive/init.sh
@@ -21,7 +21,6 @@
 sed -i '$d' /usr/local/sbin/start.sh
 sed -i '$d' /usr/local/sbin/start.sh
 cp /tmp/hive/core-site.xml /tmp/hadoop-conf
-sed -i "s|hdfs://localhost:9000|hdfs://${HIVE_HOST_IP}:9000|g" /usr/local/hive/conf/hive-site.xml
 /bin/bash /usr/local/sbin/start.sh
 hdfs dfs -mkdir -p /user/gravitino
 hdfs dfs -mkdir -p /user/iceberg/warehouse
