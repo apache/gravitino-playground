7ed9dae9db5836fd37272d3c680777972c5051b2
Eric Chang <e850506@gmail.com>
2024-12-11 08:41:11 +0800

    refactor(init/hive): use sed to replace tail -f command

--- init/hive/init.sh
+++ init/hive/init.sh
@@ -17,10 +17,9 @@
 # under the License.
 #
 
-# remove command line `tail -f /dev/null` in the `/usr/local/sbin/start.sh`, so we can run subsequent commands
-if [[ $(tail -n 1 /usr/local/sbin/start.sh) == 'tail -f /dev/null' ]]; then
-	sed -i '$d' /usr/local/sbin/start.sh
-fi
+# remove command `tail -f /dev/null` in `/usr/local/sbin/start.sh`, so we can run subsequent commands
+sed -i -E 's/tail -f \/dev\/null/\s/g' /usr/local/sbin/start.sh
+
 cp /tmp/hive/core-site.xml /tmp/hadoop-conf
 /bin/bash /usr/local/sbin/start.sh
 hdfs dfs -mkdir -p /user/gravitino
