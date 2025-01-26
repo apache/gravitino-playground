03be79663520a8cbe82e86ebb79f937622c52124
danhuawang <154112360+danhuawang@users.noreply.github.com>
2025-01-26 17:07:52 +0800

    [Minor] Update for 0.8.0 (#127)
    Update image and jar to 0.8.0 release.
     This PR is tested by 0.8.0rc3 and can be merged until 0.8.0 is announced.

--- init/spark/init.sh
+++ init/spark/init.sh
@@ -21,7 +21,7 @@ cp /tmp/spark/spark-defaults.conf /opt/spark/conf
 cp /tmp/spark/spark-env.sh /opt/spark/conf
 
 cp /tmp/spark/packages/iceberg-spark-runtime-3.4_2.12-1.5.2.jar /opt/spark/jars/iceberg-spark-runtime-3.4_2.12-1.5.2.jar
-cp /tmp/spark/packages/gravitino-spark-connector-runtime-3.4_2.12-0.7.0-incubating.jar /opt/spark/jars/gravitino-spark-connector-runtime-3.4_2.12-0.7.0-incubating.jar
+cp /tmp/spark/packages/gravitino-spark-connector-runtime-3.4_2.12-0.8.0-incubating.jar /opt/spark/jars/gravitino-spark-connector-runtime-3.4_2.12-0.8.0-incubating.jar
 cp /tmp/spark/packages/mysql-connector-java-8.0.27.jar /opt/spark/jars/mysql-connector-java-8.0.27.jar
 cp /tmp/spark/packages/kyuubi-spark-authz-shaded_2.12-1.9.2.jar /opt/spark/jars/kyuubi-spark-authz-shaded_2.12-1.9.2.jar
 sh /tmp/common/init_metalake_catalog.sh
